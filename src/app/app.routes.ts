32e123051b3674528a8e9c08f47632f36adefebc
madhu2628 <maduradevi3110@gmail.com>
2024-06-18 19:50:44 +0530

    studentsignup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { StudentLoginComponent } from './student-login/student-login.component';
 import { TeacherLoginComponent } from './teacher-login/teacher-login.component'; 
 import { MainLoginComponent } from './main-login/main-login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     
@@ -14,8 +15,13 @@ export const routes: Routes = [
         component:TeacherLoginComponent
     },
     {
+        path:'sign-up',
+        component:SignUpComponent
+    },
+    
+    {
         path:'',
         component:MainLoginComponent
     }
-    
+  
 ];
